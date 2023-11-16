6163cb0649189c8e45d0fcd63d3e829c2e73b06b
quirozariel21 <quirozariel21@gmail.com>
2023-11-16 13:36:17 -0400

    Update TranslateService.pm

--- TranslateService.pm
+++ TranslateService.pm
@@ -1,28 +1,28 @@
 package TranslateService;
 
 use Paws;
-use Paws::Credential::ProviderChain;
+use Paws::Credential::Explicit;
+use feature qw / signatures /;
+
 require './Config.pm';
 
 sub new {
     return bless {}, shift;
 }
 
-sub translate {
-# Imprimir para depuración
-   
-    my ($source_language, $target_language, $text_to_translate) = @_;
+sub translate($class, $source_language, $target_language, $text_to_translate, $aws_access_key, $aws_secret_key, $aws_session_token) {
 
-    # Configuring the Translate service with Paws
-    my $aws_config = {
-        region      => $Config::AWS_REGION, # Change this to your AWS region
-        credentials => Paws::Credential::ProviderChain->new(
-            providers => [ 'Paws::Credential::Environment', 'Paws::Credential::InstanceProfile' ],
-        ),
-    };
+    my $aws_config = Paws->new(config => {
+      credentials => Paws::Credential::Explicit->new(
+        access_key => $aws_access_key,
+        secret_key => $aws_secret_key,
+        session_token => $aws_session_token,
+      )
+    });
+
 
     # Creating an instance of the Translate service with Paws
-    my $translate = Paws->service('Translate', %$aws_config);
+    my $translate = $aws_config->service('Translate', region => $Config::AWS_REGION); # Change this to your AWS region
 
     # Performing translation using AWS Translate
     my $result = $translate->TranslateText(
