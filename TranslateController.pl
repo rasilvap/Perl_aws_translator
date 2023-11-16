ba6e570896fa71b7be572f47c9fb18dfcade8d31
quirozariel21 <quirozariel21@gmail.com>
2023-11-16 13:37:29 -0400

    Update TranslateController.pl

--- TranslateController.pl
+++ TranslateController.pl
@@ -2,6 +2,7 @@
 
 use Dancer2;
 use JSON;
+use Dancer2::Logger::Console;
 require './TranslateService.pm';
 
 set serializer => 'JSON';
@@ -11,6 +12,9 @@ any ['get', 'post'] => '/translate' => sub {
 
     # Obtaining JSON from the request body (either GET or POST)
     my $json_input = $c->request->body;
+    my $aws_access_key = request_header 'aws_access_key';
+    my $aws_secret_key = request_header 'aws_secret_key';
+    my $aws_session_token = request_header 'aws_session_token';    
 
     # Decoding the JSON
     my $input_data = decode_json($json_input);
@@ -20,9 +24,10 @@ any ['get', 'post'] => '/translate' => sub {
     my $target_language     = $input_data->{target} // '';
     my $text_to_translate   = $input_data->{text}   // '';
 
+    
     # Performing translation using the TranslateService
     my $service = TranslateService->new;
-    my $translated_text = $service->translate($source_language, $target_language, $text_to_translate);
+    my $translated_text = $service->translate($source_language, $target_language, $text_to_translate, $aws_access_key, $aws_secret_key, $aws_session_token);
 
     # Converting the response to a serializable data structure
     my $response_data = {
